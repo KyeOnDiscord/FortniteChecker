1bebd630c8f315bb1fef04dc99c94c295c1de3a6
Kye <promasterboy@outlook.com>
2023-04-17 22:59:16 +1000

    Banners

---
+++ Endpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace FortniteChecker;
+
+internal static class Endpoints
+{
+    public const string FortniteAPIURL = "https://fortnite-api.com/v2/cosmetics/br?responseOptions=ignore_null&responseFormat=msgpack_lz4";
+    public const string Banners = "https://fortnite-api.com/v1/banners?responseOptions=ignore_null&responseFormat=msgpack_lz4";
+
+}
--- Program.cs
+++ Program.cs
@@ -29,6 +29,12 @@ internal class Program
         Console.ForegroundColor = ConsoleColor.Gray;
         HttpClient httpClient = new HttpClient();
 
+
+        CosmeticsDB.CosmeticsDBRoot cosmetics = FortniteAPI.GetAllCosmetics();
+        Banners.Root banners = FortniteAPI.GetBanners();
+
+
+
         //Create fortnitePCGameClient
         var fortnitePCGameClient = new AuthClient { ClientID = "ec684b8c687f479fadea3cb2ad83f5c6", Secret = "e1f31c211f28413186262d37a13fc84d" };
 
@@ -54,8 +60,9 @@ internal class Program
         }
 
         Console.WriteLine("Welcome " + auth.displayName);
-#if DEBUG
         Console.WriteLine("Account ID " + auth.account_id);
+#if DEBUG
+
         Console.WriteLine("Your access token is: " + auth.access_token);
         Console.WriteLine("Getting QueryProfile");
         // File.WriteAllText("auth.json", auth.ToString());
@@ -63,19 +70,15 @@ internal class Program
         QueryProfile.Modal.QueryProfileRoot q = QueryProfile.Get(auth, QueryProfile.Profile.athena);
 
         var Account = q.profileChanges[0].profile;
-        Console.WriteLine("Fetching all Fortnite cosmetics");
-        string AllCosmetics = httpClient.GetStringAsync("https://fortnite-api.com/v2/cosmetics/br/").GetAwaiter().GetResult();
-        CosmeticsDB.CosmeticsDBRoot cosmetics = JsonSerializer.Deserialize(AllCosmetics, SourceGenerationContext.Default.CosmeticsDBRoot);
-        Console.WriteLine("Fetched all cosmetics, count: " + cosmetics.data.Length);
 
         List<CosmeticsDB.Datum> ownedCosmetics = new();
-        string[] CosmeticItemsToSearch = { "AthenaCharacter", "AthenaBackpack", "AthenaDance", "AthenaPickaxe", "AthenaGlider", "AthenaItemWrap", "AthenaLoadingScreen", "AthenaMusicPack", "AthenaSkyDiveContrail", "AthenaSpray" };
+        string[] CosmeticItemsToSearch = { "AthenaCharacter", "AthenaBackpack", "AthenaDance", "AthenaPickaxe", "AthenaGlider", "AthenaItemWrap", "AthenaLoadingScreen", "AthenaMusicPack", "AthenaSkyDiveContrail" };
         foreach (var item in Account.items)
         {
-            string itemName = item.Value.templateId;
+            string[] itemName = item.Value.templateId.Split(':');
 
-            string itemType = itemName.Split(':')[0];
-            string itemValue = itemName.Split(':')[1];
+            string itemType = itemName[0];
+            string itemValue = itemName[1];
             if (CosmeticItemsToSearch.Contains(itemType))
             {
                 var cosmetic = cosmetics.data.FirstOrDefault(x => x.id.ToLower() == itemValue);
@@ -84,11 +87,13 @@ internal class Program
 
                 if (cosmetic.introduction == null)
                 {
-                    cosmetic.introduction = new CosmeticsDB.Introduction();
-                    cosmetic.introduction.text = "Unknown";
-                    cosmetic.introduction.backendValue = 0;
-                    cosmetic.introduction.season = "1";
-                    cosmetic.introduction.chapter = "1";
+                    cosmetic.introduction = new()
+                    {
+                        text = "Unknown",
+                        backendValue = 0,
+                        season = "1",
+                        chapter = "1"
+                    };
                 }
                 cosmetic.rarity.backendIntValue = Rarity.RarityToInt(cosmetic.rarity.value);
                 ownedCosmetics.Add(cosmetic);
@@ -128,13 +133,25 @@ internal class Program
         currentSeason.numWins = Account.stats.attributes.lifetime_wins - PastWinCount;
         currentSeason.seasonLevel = Account.stats.attributes.level;
         currentSeason.bookLevel = Account.stats.attributes.book_level;
-        currentSeason.purchasedVIP = false;
+        currentSeason.purchasedVIP = Account.stats.attributes.book_purchased;
 
 
         pastSeasons.Add(currentSeason);
         QueryProfile.Modal.QueryProfileRoot common = QueryProfile.Get(auth, QueryProfile.Profile.common_core);
 
         int VBucks = GetVbucks(common);
+
+
+        var OwnedBanners = GetBanners(common);
+        List<Banners.Datum> ActuallyOwnedBanners = new();
+
+        foreach (var item in OwnedBanners)
+        {
+            var banner = banners.data.FirstOrDefault(x => x.id.ToLower() == item.ToLower());
+            if (banner != null)
+                ActuallyOwnedBanners.Add(banner);
+        }
+
         string GiftsSent = "0";
         string GiftsReceived = "0";
 
@@ -149,26 +166,29 @@ internal class Program
 
         string lifetimewins = Account.stats.attributes.lifetime_wins.ToString();
         string timestamp = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
-        string data = JsonBuilder.CreateJsonFile(ownedCosmetics, pastSeasons, VBucks.ToString(), GiftsSent, GiftsReceived, timestamp, auth.displayName, lifetimewins, EpicID);
+        string data = JsonBuilder.CreateJsonFile(ownedCosmetics, pastSeasons, ActuallyOwnedBanners, VBucks.ToString(), GiftsSent, GiftsReceived, timestamp, auth.displayName, lifetimewins, EpicID);
 
         HttpContent content = new StringContent(data, Encoding.UTF8, "text/json");
         content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-        //#if DEBUG
+#if DEBUG
 
-        //        HttpResponseMessage resp = httpClient.PostAsync("http://localhost:3000/file", content).GetAwaiter().GetResult();
-        //        string messageID = resp.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-        //        string CheckerLink = $"http://localhost:3000/?file={messageID}";
-        //        Process.Start(new ProcessStartInfo() { FileName = CheckerLink, UseShellExecute = true });
-        //#endif
+        File.WriteAllText(Account._id, data);
 
-        //#if RELEASE
+        HttpResponseMessage resp = httpClient.PostAsync("https://checker.proswapper.xyz/file", content).GetAwaiter().GetResult();
+        string messageID = resp.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+        string CheckerLink = $"http://localhost:3000/?file={messageID}";
+        Process.Start(new ProcessStartInfo() { FileName = CheckerLink, UseShellExecute = true });
+#endif
+
+
+#if RELEASE
         HttpResponseMessage resp = httpClient.PostAsync("https://checker.proswapper.xyz/file", content).GetAwaiter().GetResult();
         string messageID = resp.Content.ReadAsStringAsync().GetAwaiter().GetResult();
 
         string CheckerLink = $"https://checker.proswapper.xyz/?file={messageID}";
         CheckerLink = "https://link-to.net/86737/" + new Random().Next(0, 1000).ToString() + "/dynamic/?r=" + Base64Encode(CheckerLink);
         Process.Start(new ProcessStartInfo() { FileName = CheckerLink, UseShellExecute = true });
-        //#endif
+#endif
 
 
 
@@ -179,6 +199,11 @@ internal class Program
         int VBucks = 0;
         foreach (var item in common.profileChanges[0].profile.items)
         {
+            //MtxComplimentary
+            //MtxGiveaway
+            //MtxPurchaseBonus
+            //MtxPurchased
+
             if (item.Value.templateId.StartsWith("Currency:Mtx"))
             {
                 if (item.Value != null && item.Value.quantity != null)
@@ -188,6 +213,20 @@ internal class Program
             }
         }
         return VBucks;
+    }
+
+    private static List<string> GetBanners(QueryProfile.Modal.QueryProfileRoot common)
+    {
+        List<string> Banners = new();
+        foreach (var item in common.profileChanges[0].profile.items)
+        {
+
+            if (item.Value.templateId.StartsWith("HomebaseBannerIcon:"))
+            {
+                Banners.Add(item.Value.templateId.Split(':')[1]);
+            }
+        }
+        return Banners;
     }
 
     public static byte[] GetHash(string inputString)
